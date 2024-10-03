d9d4025fe2fbb5f1e76cea0b31f09ded8302f928
ashrafbel <ashrafmus2@gmail.com>
2024-10-03 14:02:12 -0700

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const r1 = new ClassRoom(19);
+  const r2 = new ClassRoom(20);
+  const r3 = new ClassRoom(34);
+
+  return [r1, r2, r3];
+}
+
