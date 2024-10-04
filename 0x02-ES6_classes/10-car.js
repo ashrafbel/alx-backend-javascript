69d0452a2ae66cf03691825f45ad1d82734460c5
ashrafbel <ashrafmus2@gmail.com>
2024-10-03 17:06:39 -0700

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,19 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const clone = new this.constructor();
+    Object.assign(clone, {
+      _brand: this._brand,
+      _motor: this._motor,
+      _color: this._color,
+    });
+    return clone;
+  }
+}
