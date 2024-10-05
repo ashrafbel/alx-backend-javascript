0ba7ef41eec744bcd5fcdfb7f7788199d411026e
ashrafbel <ashrafmus2@gmail.com>
2024-10-04 17:29:27 -0700

    100-

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,10 @@ export default function createIteratorObject(report) {
   const employees = [];
 
   for (const department in report.allEmployees) {
-    employees.push(...report.allEmployees[department]);
+    // Use Object.prototype.hasOwnProperty to avoid issues with shadowing
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+      employees.push(...report.allEmployees[department]);
+    }
   }
 
   return employees[Symbol.iterator]();
