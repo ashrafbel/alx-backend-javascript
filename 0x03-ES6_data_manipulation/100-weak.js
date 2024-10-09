0c418ef5008fbc25e1bf3e0bcc521f7b52cd6a07
ashrafbel <ashrafmus2@gmail.com>
2024-10-08 18:14:04 -0700

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
+// 100-weak.js
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   let count = weakMap.get(endpoint) || 0;
-  count++;
-  
+  count += 1;
+
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-  
+
   weakMap.set(endpoint, count);
-  return;
 }
