e905544ac35d65fce76943d3f1188705ed324a48
ashrafbel <ashrafmus2@gmail.com>
2024-11-26 22:05:57 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,7 @@
 const readline = require('readline');
-
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
