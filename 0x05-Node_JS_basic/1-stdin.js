5e62645339b9888717d0648323cff9b4242c3642
ashrafbel <ashrafmus2@gmail.com>
2024-11-26 22:14:49 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  
   if (name !== null) {
     const trimmedName = name.toString().trim();
-    
     if (trimmedName) {
       process.stdout.write(`Your name is: ${trimmedName}\n`);
     }
-    
     process.stdout.write('This important software is now closing\n');
     process.exit();
   }
