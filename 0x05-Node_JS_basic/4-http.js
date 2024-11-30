8029f6f808710444948cf91191e854d89f8b536c
ashrafbel <ashrafmus2@gmail.com>
2024-11-30 00:52:08 +0000

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,18 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+  // Set the response header with plain text content type
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  res.end('Hello Holberton School!\n');
+  
+  // Send the response body
+  res.end('Hello Holberton School!');
 });
 
+// Server listens on port 1245
 app.listen(1245, () => {
-  console.log('Server listening on port 1245');
+  // Optional: log that the server is running (not required by specifications)
+  console.log('Server running on port 1245');
 });
 
+// Export the app as required
 module.exports = app;
