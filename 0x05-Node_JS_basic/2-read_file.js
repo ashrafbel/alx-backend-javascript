fe53318dc0c92ed208debffd1962997fa5f413f2
ashrafbel <ashrafmus2@gmail.com>
2024-11-30 00:20:16 +0000

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length === 0) throw new Error('Cannot load the database');
+
+    const header = lines[0].split(',');
+
+    const fieldIndex = header.indexOf('field');
+    const firstNameIndex = header.indexOf('firstname');
+
+    if (fieldIndex === -1 || firstNameIndex === -1) {
+      throw new Error('Invalid CSV format');
+    }
+
+    const students = lines.slice(1).map((line) => {
+      const fields = line.split(',');
+      return {
+        firstname: fields[firstNameIndex].trim(),
+        field: fields[fieldIndex].trim(),
+      };
+    });
+
+    const validStudents = students.filter((student) => student.firstname && student.field);
+    console.log(`Number of students: ${validStudents.length}`);
+
+    const fieldsMap = {};
+    validStudents.forEach((student) => {
+      if (!fieldsMap[student.field]) {
+        fieldsMap[student.field] = [];
+      }
+      fieldsMap[student.field].push(student.firstname);
+    });
+
+    for (const field in fieldsMap) {
+      console.log(
+        `Number of students in ${field}: ${fieldsMap[field].length}. List: ${fieldsMap[field].join(', ')}`
+      );
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
